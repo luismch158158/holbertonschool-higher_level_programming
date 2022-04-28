0defbc6dcd6bd81d3f34a7e1bb3a98b958643d97
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-04-28 15:19:55 -0500

    Adding program that imports all functions and handles basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    longitud = len(sys.argv)
+    items = 0
+
+    if (longitud != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    item_1 = int(sys.argv[1])
+    item_2 = int(sys.argv[3])
+    operator = sys.argv[2]
+
+    struct = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if operator not in struct:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print(f'{item_1} {operator} {item_2} = {struct[operator](item_1, item_2)}')
