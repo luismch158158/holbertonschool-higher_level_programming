38b8b9a1b62b2874872a404909e80b5ff33c11e2
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-02 13:01:34 -0500

    Fix addition condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return None
-    else:
+    if my_list:
         my_list.reverse()
         for i in range(len(my_list)):
             print(f'{my_list[i]}')
