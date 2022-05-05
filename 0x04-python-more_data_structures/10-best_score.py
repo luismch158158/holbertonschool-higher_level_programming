ab9b1fddc971f9ab010b4aa93a981a03dcbdfde4
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-04 19:18:05 -0500

    Fix return none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    new_max = max(a_dictionary, key=a_dictionary.get)
-    return new_max
+    if a_dictionary is not None:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
+
