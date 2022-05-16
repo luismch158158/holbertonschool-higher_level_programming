d33df99d31f64b744ee33e01f3a41fcbff4e0f46
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-16 14:01:51 -0500

    Fix range of the list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     j = 0
-    for i in my_list:
+    for i in range(0, x):
         try:
-            if x > j:
-                print(f'{i}', end="")
-            j = j + 1
+            print(f'{my_list[i]}', end="")
+            j += 1
         except (IndexError):
             continue
     print()
