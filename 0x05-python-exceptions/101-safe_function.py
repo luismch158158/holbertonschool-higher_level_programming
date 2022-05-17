6d873e02a1c5a150a1a217bc142ee2e7739462de
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-16 20:00:30 -0500

    Modified type of error with Exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,9 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return (fct(args[0], args[1]))
-    except ZeroDivisionError as ne:
+    except Exception as ne:
         sys.stderr.write("Exception: {}\n".format(ne))
-        return None
-    except IndexError as ni:
-        sys.stderr.write("Exception: {}\n".format(ni))
         return None
