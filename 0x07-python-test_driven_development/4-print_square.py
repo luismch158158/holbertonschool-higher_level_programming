f0163ea0899d6896ab92e9cdef1cba4fa9e8f48b
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-20 23:55:48 -0500

    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This program print squares of #"""
+
+
+def print_square(size):
+    """Print square with the character # in stdout"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print()
+    else:
+        j = 0
+        while j < size:
+            print("#" * size)
+            j += 1
