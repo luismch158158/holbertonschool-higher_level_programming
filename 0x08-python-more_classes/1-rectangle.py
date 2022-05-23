402b0ef778384ef9c4c5be8907d9974108b47af9
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-23 17:36:23 -0500

    Adding private instance attribute width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Class Rectangle empty"""
+
+
+class Rectangle:
+    """Defines a Rectangle
+    with Private instance attribute width
+    and Private instance attribute height"""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Getter to width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Setter to width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter to width"""
+        return (self.__height)
+    
+    @height.setter
+    def height(self, value):
+        """Setter to width"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
