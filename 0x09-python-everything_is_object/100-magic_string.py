c1b621f9d0af2fdcd8145058bac0f79a8083116f
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-24 18:34:58 -0500

    Fix typo in the string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(container={"counter": 0}):
     container["counter"] += 1
-    return (", ".join(["Best School"] * (container["counter"])))
+    return (", ".join(["BestSchool"] * (container["counter"])))
