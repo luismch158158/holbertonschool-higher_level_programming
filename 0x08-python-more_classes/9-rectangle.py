46280c217e70ae1fc10efd6110baebcba68cc583
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-23 19:32:21 -0500

    Change order of height and width declaration

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Class Rectangle empty"""
 
 
@@ -12,8 +13,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
