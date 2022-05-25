f634f004206a862510cadcf948eaf084e2cde077
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-25 09:44:01 -0500

    Adding doc in function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,6 +6,14 @@ after each of these characters: ., ? and :
 
 
 def text_indentation(text):
+    """
+    Function that prints a text
+    with 2 new lines after each of these characters:
+    - "."
+    - "?"
+    - ":"
+    """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
