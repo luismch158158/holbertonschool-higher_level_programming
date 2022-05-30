2993ca579372a5d70e793dec1d8d23943d884b4f
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-30 18:49:05 -0500

    Change order with integer validation of width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Instantiation with width and height"""
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
