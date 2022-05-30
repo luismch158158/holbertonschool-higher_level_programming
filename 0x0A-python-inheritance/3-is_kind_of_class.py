3d44f7a451cf7dac226a90ef2a2c6656a20ada74
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-30 18:47:12 -0500

    Fix pycodestyle

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class definition"""
 
+
 def is_kind_of_class(obj, a_class):
     """
     Function that returns True, if the object of,
@@ -8,4 +9,4 @@ def is_kind_of_class(obj, a_class):
     that inherited from, the specifies class,
     otherwise False
     """
-    return (isinstance(obj, a_class))
+    return isinstance(obj, a_class)
