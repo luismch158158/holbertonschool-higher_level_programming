cb7b137847ee670f01f75b37aeadef4c77c8bfed
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-30 16:51:35 -0500

    Only subclass of, with isinstance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class definition"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class, otherwise False"""
+    if type(obj) == a_class:
+        return False
+    return (isinstance(obj, a_class))
