98bc22fa873ac3f1225e0735e0a83945f0fba194
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-31 15:50:30 -0500

    Fix filename in the script

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """This function that reads a text file (UTF8)
     and prints it to stdout"""
 
-    with open("my_file_0.txt", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
