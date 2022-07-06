4a9f480a2dc6a230eba79336b243bc18cf5ab250
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-06 16:37:55 -0500

    fix personalize output alias

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Script that lists all cities contained in the database hbtn_0d_usa
 
-SELECT cities.id AS id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
     FROM cities AS c
     INNER JOIN states AS s
     ON c.state_id = s.id
