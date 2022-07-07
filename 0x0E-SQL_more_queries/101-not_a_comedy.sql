140ec6e51d7d8c9ea8edbe5812c8bf1494179f7f
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-06 20:08:19 -0500

    script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- Script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+
+SELECT DISTINCT tv_shows.title FROM tv_shows
+    LEFT JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_shows.title NOT IN (
+        SELECT tv_shows.title FROM tv_genres
+        INNER JOIN tv_show_genres
+            ON tv_genres.id = tv_show_genres.genre_id
+        INNER JOIN tv_shows
+            ON tv_show_genres.show_id = tv_shows.id
+        WHERE tv_genres.name = "Comedy"
+    )
+    ORDER BY tv_shows.title ASC;
