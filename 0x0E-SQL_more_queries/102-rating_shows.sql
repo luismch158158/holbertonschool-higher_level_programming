b7cf4e2a401ec03e3138d1b3529b37658a28ee36
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-06 20:14:56 -0500

    script that lists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Script that lists all shows from hbtn_0d_tvshows_rate by their rating
 
-SELECT tv.shows.tittle, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
+SELECT tv_shows.tittle, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
     INNER JOIN tv_show_ratings
         ON tv_shows.id = tv_show_ratings.show_id
     GROUP BY tv_shows.tittle
