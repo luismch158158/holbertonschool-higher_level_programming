793c19b5076b2d9b5421729fe45558ef669bcc5e
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-02 00:39:02 -0500

    Fixing utf8

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,18 +7,18 @@ const { argv } = require('process');
 
 let content = '';
 
-content += fs.readFileSync(argv[2], 'utf-8', (err, data) => {
+content += fs.readFileSync(argv[2], 'utf8', (err, data) => {
   if (err) console.error(err);
 });
 
 content += '\n';
 
-content += fs.readFileSync(argv[3], 'utf-8', (err, data) => {
+content += fs.readFileSync(argv[3], 'utf8', (err, data) => {
   if (err) console.error(err);
 });
 
 content += '\n';
 
-fs.writeFileSync(argv[4], content, 'utf-8', err => {
+fs.writeFileSync(argv[4], content, 'utf8', err => {
   if (err) console.error(err);
 });
