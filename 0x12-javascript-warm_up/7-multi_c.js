d737d840d70ef0f53ef419beef18ed02301745cd
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:20:03 -0500

    prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+const process = require('process');
+
+const args = process.argv;
+let numberTimes = Number.parseInt(args[2]);
+
+if (Number.isNaN(numberTimes)) {
+  console.log('Missing number of occurrences');
+}
+
+while (numberTimes > 0) {
+  console.log('C is fun');
+  numberTimes--;
+}
