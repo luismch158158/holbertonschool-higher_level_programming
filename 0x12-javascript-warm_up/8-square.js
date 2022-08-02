d96ae5e88475ab65c9a0d5e2a5aadd79006bd168
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:20:14 -0500

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const process = require('process');
+
+const args = process.argv;
+const numberTimes = Number.parseInt(args[2]);
+
+if (Number.isNaN(numberTimes)) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < numberTimes; i++) {
+  for (let j = 0; j < numberTimes; j++) {
+    process.stdout.write('X');
+  }
+  console.log();
+}
