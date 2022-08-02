44ceb79a7b4bd7235609bcbd9cd892713bb91696
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:21:03 -0500

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,27 @@
 #!/usr/bin/node
 
+const process = require('process');
+const args = process.argv;
+const longitude = process.argv.length - 2;
+
+const newArray = [];
+let arraySet;
+let finalArray;
+
+for (let i = 0; i < longitude; i++) {
+  newArray[i] = parseInt(args[i + 2]);
+}
+
+if (longitude === 1 || longitude === 0) {
+  console.log(0);
+} else {
+  arraySet = new Set(newArray);
+  finalArray = Array.from(arraySet).sort(function (a, b) {
+    return b - a;
+  });
+  if (finalArray.length === 1) {
+    console.log(finalArray[0]);
+  } else {
+    console.log(finalArray[1]);
+  }
+}
