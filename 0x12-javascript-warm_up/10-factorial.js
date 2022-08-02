7c2d2ae4d7b6821e7bfc82197c76ff0b4fa51b64
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-02 00:12:51 -0500

    fixing semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,16 +6,14 @@ const args = process.argv;
 
 const number = parseInt(args[2]);
 
-if (Number.isNaN(number)){
-  console.log(1);
-  return (0);
-}
-
 function factorial (number) {
-  if (number <= 1){
+  if (Number.isNaN(number)) {
+    // console.log(1);
+    return (1);
+  } else if (number <= 1) {
     return 1;
   }
-  return (number * factorial (number - 1));
+  return (number * factorial(number - 1));
 }
 
 console.log(factorial(number));
