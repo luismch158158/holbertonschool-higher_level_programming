4ddc25dd94a6bf5496f8b7d89935ad605f5104ae
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:20:32 -0500

    prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+const process = require('process');
+
+const args = process.argv;
+
+function add (a, b) {
+  console.log(`${a + b}`);
+}
+
+const a = parseInt(args[2]);
+const b = parseInt(args[3]);
+
+add(a, b);
