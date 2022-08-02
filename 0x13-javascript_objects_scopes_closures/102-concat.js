a7f833fcb0cb27883c3789e26cd901f709b90713
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-02 00:49:09 -0500

    fixing semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,16 +5,16 @@ const fs = require('fs');
 
 const args = process.argv;
 
-let content1 = fs.readFileSync(args[2], 'utf8', (err, data) => {
+const content1 = fs.readFileSync(args[2], 'utf8', (err, data) => {
   if (err) console.error(err);
 });
 
-let content2 = fs.readFileSync(args[3], 'utf8', (err, data) => {
+const content2 = fs.readFileSync(args[3], 'utf8', (err, data) => {
   if (err) console.error(err);
 });
 
-const final_content = content1 + content2;
+const finalContent = content1 + content2;
 
-fs.writeFileSync(args[4], final_content, 'utf8', err => {
+fs.writeFileSync(args[4], finalContent, 'utf8', err => {
   if (err) console.error(err);
 });
