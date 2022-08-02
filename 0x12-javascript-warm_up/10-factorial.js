d76a0d49259f565b84cdabe8a9f3d249a5a070cc
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:20:47 -0500

    factorial in js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,21 @@
 #!/usr/bin/node
 
+const process = require('process');
+
+const args = process.argv;
+
+const number = parseInt(args[2]);
+
+if (Number.isNaN(number)){
+  console.log(1);
+  return (0);
+}
+
+function factorial (number) {
+  if (number <= 1){
+    return 1;
+  }
+  return (number * factorial (number - 1));
+}
+
+console.log(factorial(number));
