6d2c663f65522d13f4cbf6a57c52e6c3d5cc585c
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:19:00 -0500

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+const process = require('process');
+
+const args = process.argv;
+const longitude = args.length - 2;
+
+if (longitude === 0) {
+  console.log('No argument');
+} else if (longitude === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
