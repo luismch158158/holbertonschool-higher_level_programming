043ac14f0414af4db8ae6573c8c3b34aaf8e9dd2
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:19:12 -0500

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 
+const process = require('process');
+
+const args = process.argv;
+let counter = 0;
+
+const counterFunction = () => counter++;
+
+args.forEach(counterFunction);
+
+if (counter === 2) {
+  console.log('No argument');
+} else {
+  console.log(`${args[2]}`);
+}
