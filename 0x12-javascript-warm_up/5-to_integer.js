a65783dafe668d6219bf8ca0c442d99c2f5c902b
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-01 19:19:38 -0500

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+const process = require('process');
+const args = process.argv;
+
+const newNumber = Number.parseInt(args[2]);
+
+if (Number.isNaN(newNumber)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${newNumber}`);
+}
