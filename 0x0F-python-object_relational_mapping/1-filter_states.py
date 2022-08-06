09f4461b0a54e5ef2125a6875b44570cd9ad49ad
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-06 08:31:05 -0500

    fixing query adding case sensity in the query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,17 +7,13 @@ if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
-    try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
-    except Exception:
-        print("Can't connect to database")
-        exit(0)
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE 'N%' ORDER BY id ASC;")
+                    WHERE name LIKE BINARY 'N%' ORDER BY id ASC;")
 
     m = cursor.fetchall()
 
