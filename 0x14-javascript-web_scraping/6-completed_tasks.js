340a0225a7773a00cbcfd597777920a99303f7ab
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-13 16:25:18 -0500

    Fix with all tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 const { get } = require('axios').default;
 const urlBase = process.argv[2];
-// const complement = "?userId=1&completed=true"
 
-get(`${urlBase}?completed=true`)
+
+get(urlBase)
+  // data es un array de objetos
   .then(({ data }) => {
     const finalObj = {};
-    for (const element of data) {
-      const id = element.userId;
-      if (finalObj[id]) {
-        finalObj[id] += 1;
-      } else {
-        finalObj[id] = 1;
+    data.forEach(({ userId, completed }) => {
+      if (completed) {
+        if (finalObj[userId] === undefined) {
+          finalObj[id] = 1;
+        } else {
+          finalObj[id] += 1;
+        }
       }
-    }
+    })
     console.log(finalObj);
   })
   .catch((err) => console.log(err));
