a527468d3422dd84c133aac59e0541e82f041741
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-13 15:57:02 -0500

    changing to filter with id

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 const { get } = require('axios').default;
 const url = process.argv[2];
-const urlChar = 'https://swapi-api.hbtn.io/api/people/18/';
 
 get(url)
   .then(({ data }) => data)
   .then(({ results }) => {
     let count = 0;
     for (const film of results) {
-      if (film.characters.some(person => urlChar === person)) { count += 1; }
+      film.characters.forEach(urls => {
+        if (urls.includes('18')) {
+          count += 1
+        }
+      });
     }
     console.log(count);
   })
